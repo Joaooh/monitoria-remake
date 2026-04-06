5c44102c8a940888b3c8e091445449665a9184f0
Joaooh <83705635+Joaooh@users.noreply.github.com>
2026-04-06 00:52:31 -0300

    chore: pequenas otimizações e melhorias de SEO

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,10 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
-
-import react from '@astrojs/react';
-
-import sitemap from '@astrojs/sitemap';
+import { defineConfig } from "astro/config";
+import react from "@astrojs/react";
+import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://monitoriadeti.vercel.app', // Substituir pelo domínio oficial depois
+  site: "https://monitoria-remake.vercel.app",
   integrations: [react(), sitemap()],
 });
